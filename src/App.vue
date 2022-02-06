f4fdd81edadcd0ef574ba36aeceab6b1136f3c1a
veeru-neerukonda <veeru.neerukonda@gmail.com>
2022-02-06 10:53:53 +0530

    added in font settings for the project and have started work on the main-page.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,35 @@
 <template>
-    Hello World
+    <div class="frame">
+        <div class="icon-area"></div>
+        <div class="top-bar"></div>
+        <div class="left-bar"></div>
+        <div class="content-area"></div>
+    </div>
 </template>
+
+<style lang="scss">
+    @import '@/assets/scss/reset.scss';
+    @import '@/assets/scss/_settings.scss';
+    @import '@/assets/scss/_breakpoints.scss';
+
+    .frame{
+        display: grid;
+        grid-template-columns: 50px 1fr;
+        grid-template-rows: 50px 1fr;
+        grid-template-areas: 
+            "icon-area top-bar"
+            "left-bar content-area";
+        
+        height: 100vh;
+
+        @include for-tablet-portrait-up{
+            grid-template-columns: 100px 1fr;
+            grid-template-rows: 100px 1fr;
+        }
+    }
+
+    .icon-area{
+        grid-area: icon-area;
+        
+    }
+</style>
