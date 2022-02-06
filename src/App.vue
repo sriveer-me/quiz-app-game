52945d04419877a281957e4c110e5568da3024c2
veeru-neerukonda <veeru.neerukonda@gmail.com>
2022-02-06 12:31:42 +0530

    refactored out the top-bar as a seperate component.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
 <template>
     <div class="frame">
         <h1 class="icon-area icon-font">QZ</h1>
-        <div class="top-bar">
-            <div class="top-bar-box time-box">
-                <h2 class="subtitle-font">Time</h2>
-                <h3 class="body-text">0:00</h3>
-            </div>
-            <div class="top-bar-box score-box">
-                <h2 class="subtitle-font">Score</h2>
-                <div class="score-box__internal">
-                    <h3 class="subtitle-font light-color">0</h3>
-                </div>
-            </div>
-        </div>
+        <TopBar />
         <div class="left-bar"></div>
         <div class="content-area"></div>
     </div>
@@ -51,75 +40,18 @@
         justify-content: center;
         align-items: center;
     }
-
-    .top-bar{
-        grid-area: top-bar;
-        border: 1px solid $dark;
-        padding: 0 var(--spacing-large);
-
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-
-        .time-box{
-            display: flex;
-
-            flex-direction: row;
-            column-gap: var(--spacing-small);
-            row-gap: 0;
-            align-items: flex-end;
-            h3{
-                margin-bottom: 1px;
-            }
-
-            @include for-tablet-portrait-up{
-                flex-direction: column;
-                column-gap: 0;
-                row-gap: var(--spacing-small);
-                align-items: center;
-                h3{
-                    margin-bottom: 0;
-                }
-            }
-        }
-
-        .score-box{
-            display: flex;
-            flex-direction: row;
-            column-gap: var(--spacing-small);
-
-            .score-box__internal{
-                border-radius: 100%;
-                width: 32px;
-                height: 32px;
-                background-color: $dark;
-
-                display: flex;
-                justify-content: center;
-
-                @include for-tablet-portrait-up{
-                    width: 64px;
-                    height: 64px;
-
-                    align-items: center;
-                    h3.subtitle-font{ //this is important for specificy reasons
-                        font-size: 64px;
-                    }
-                }
-            }
-
-            @include for-tablet-portrait-up{
-                align-items: center;
-            }
-        }
-    }
 </style>
 
 <script>
 import {isMobile,isTablet,isDesktop} from '@/js/breakpoints.js';
 
+import TopBar from '@/components/TopBar.vue';
+
 export default {
     name: "App",
+    components:{
+        TopBar
+    },
     data(){
         return{
             isMobile: true,
---
+++ src/components/TopBar.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="top-bar">
+        <div class="top-bar-box time-box">
+            <h2 class="subtitle-font">Time</h2>
+            <h3 class="body-text">0:00</h3>
+        </div>
+        <div class="top-bar-box score-box">
+            <h2 class="subtitle-font">Score</h2>
+            <div class="score-box__internal">
+                <h3 class="subtitle-font light-color">0</h3>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    @import '@/assets/scss/_settings.scss';
+    @import '@/assets/scss/_breakpoints.scss';
+
+    .top-bar{
+        grid-area: top-bar;
+        border: 1px solid $dark;
+        padding: 0 var(--spacing-large);
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .time-box{
+            display: flex;
+
+            flex-direction: row;
+            column-gap: var(--spacing-small);
+            row-gap: 0;
+            align-items: flex-end;
+            h3{
+                margin-bottom: 1px;
+            }
+
+            @include for-tablet-portrait-up{
+                flex-direction: column;
+                column-gap: 0;
+                row-gap: var(--spacing-small);
+                align-items: center;
+                h3{
+                    margin-bottom: 0;
+                }
+            }
+        }
+
+        .score-box{
+            display: flex;
+            flex-direction: row;
+            column-gap: var(--spacing-small);
+
+            .score-box__internal{
+                border-radius: 100%;
+                width: 32px;
+                height: 32px;
+                background-color: $dark;
+
+                display: flex;
+                justify-content: center;
+
+                @include for-tablet-portrait-up{
+                    width: 64px;
+                    height: 64px;
+
+                    align-items: center;
+                    h3.subtitle-font{ //this is important for specificy reasons
+                        font-size: 64px;
+                    }
+                }
+            }
+
+            @include for-tablet-portrait-up{
+                align-items: center;
+            }
+        }
+    }
+</style>
