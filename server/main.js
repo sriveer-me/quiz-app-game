a74bea23ecdc7ab66fe84bbf23a357e8664c05b7
veeru-neerukonda <veeru.neerukonda@gmail.com>
2022-02-18 08:57:37 +0530

    started writing the api for the server.

--- server/main.js
+++ server/main.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const highScoreFilePath = path.join(__dirname,"./highScores.json")
+
 let express = require('express');
 const app = express();
 
@@ -6,8 +11,17 @@ app.listen(port,function(){
     console.log(`server listening on port : ${port}`);
 })
 
+
+// //get the top 5 scores from the server
 app.get('/',function(req,res){
-    res.json({
-        msg: "Hello World"
-    });
+    const data = fs.readFileSync(highScoreFilePath,
+        {encoding:'utf8', flag:'r'}); 
+    
+    let highScoreObj = JSON.parse(data);
+    res.json(highScoreObj);
 })
+
+// //put a new score into the server
+// app.put('/score',function(req,res){
+
+// })
